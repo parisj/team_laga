ead82c2043a4bd38a4d0ce58f795e4ac31e50012
Alexander Bruun <alex@bruun.xyz>
2022-11-11 23:09:54 +0100

    Fick load_data

--- code/load_data.py
+++ code/load_data.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 
 zonen_30 = pd.read_csv("data/tempo-30-zonen.csv", sep=";")
 begegnungszonen = pd.read_csv("data/begegnungszonen.csv", sep=";")
-gemeindestrasse = pd.read_csv("data/gemeindestrassenplan.csv", sep=";")
+gemeindestrassen = pd.read_csv("data/gemeindestrassenplan.csv", sep=";")
 
-zonen_30_js = json.loads(df['Geo Shape'][0])
-data = np.array(js['coordinates'][0])
+begegnungszonen_js = json.loads(gemeindestrassen['Geo Shape'][0])
+data = np.array(begegnungszonen_js['coordinates'][0])
 fig, ax = plt.subplots()
 ax.plot(data[:, 0], data[:, 1], ls="-", marker="o")
 fig.savefig("/tmp/test.png")
