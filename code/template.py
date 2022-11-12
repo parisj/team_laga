7e2c3ea909fbb709eea009040c04980d3cfc1a6e
Anton Paris <anton.j.paris@gmail.com>
2022-11-12 13:14:07 +0100

    adding ploting polygon and multipolygon

--- code/template.py
+++ code/template.py
@@ -2,18 +2,48 @@ import json
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from shapely.geometry import Point, LineString, Polygon, MultiPolygon
+from shapely.geometry import Point, LineString, Polygon, MultiPolygon, shape
+from shapely.validation import make_valid
 
 
-def plot_poly(multipoly):
+def plot_multipoly(multipoly):
     print(multipoly)
     fig, ax = plt.subplots()
     for poly in multipoly:
+        print(poly)
         x,y=poly.exterior.xy
         ax.plot(x, y)
      
     plt.show()
     
+def plot_poly(poly):
+    x,y=poly.exterior.xy
+    plt.plot(x, y)
+    plt.show()
+    
+def create_multipolygon_intersection(file_path, multipoly):
+    df_area = pd.read_csv(str(file_path), sep=";")
+    js_area_s = json.loads(df_area['Geo Shape'][0])
+    area_np = np.array(js_area_s['coordinates'][0])
+    poly_area = Polygon (area_np)
+    plot_poly(poly_area)
+    i=0
+    for zone in df_area['Geo Shape']:
+        #print (zone, 'ZONE')
+        js_points = json.loads(zone)
+        points = np.array(js_points['coordinates'][0])
+        if points.ndim == 3:
+            points = points[0]
+        print(points, 'points')
+        print(points.shape, 'shape')
+        i+=1
+        print(i)
+        poly_area_new = Polygon(tuple(points))
+        if multipoly.intersects(poly_area_new):
+            print(multipoly.intersects(poly_area_new))
+            poly_area=poly_area.union(poly_area_new)
+        
+    return poly_area
 def create_multipolygon(file):
     df_area = pd.read_csv(str(file), sep=";")
     js_area_s = json.loads(df_area['Geo Shape'][0])
@@ -66,7 +96,7 @@ def import_intersection(data_street, *args):
         if arg != args[0]:
             poly_area= poly_area.union(create_multipolygon(arg))
         
-    plot_poly(poly_area)
+    plot_multipoly(poly_area)
     #fig, ax = plt.subplots()
     #for poly in poly_area:
     #    x,y=poly.exterior.xy
@@ -89,14 +119,13 @@ def import_intersection(data_street, *args):
     # #print(area, 'appended')
     # polygon_area = Polygon(area)
     # print(polygon_area, 'AREA')
-  
-    
-    
     #poly_street = Polygon(street)
-    poly_street = create_multipolygon(data_street)
-    intersection = poly_area.intersection(poly_street)
+    
+    poly_street = create_multipolygon_intersection(data_street, poly_area)
+    intersection_raw = poly_area.intersection(poly_street)
+    intersection= make_valid(intersection_raw)
     print(intersection, 'intersection')
-    plot_poly(intersection)
+    plot_multipoly(intersection)
   
     return intersection
 
