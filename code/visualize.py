68e021529fc01cbebe3c59fce2d6026220054d52
einundzwanziger <102036929+einundzwanziger@users.noreply.github.com>
2022-11-12 23:02:37 +0100

    c

--- code/visualize.py
+++ code/visualize.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 import geopandas as gpd
 
 
-path_data_30 = "code/data/tempo-30-zonen.csv"
-path_strassenplan = "code/data/gemeindestrassenplan.csv"
-path_begegnungszonen = "code/data/begegnungszonen.csv"
+path_data_30 = "data/tempo-30-zonen.csv"
+path_strassenplan = "data/gemeindestrassenplan.csv"
+path_begegnungszonen = "data/begegnungszonen.csv"
+
 intersection = tp.import_intersection(path_strassenplan, path_begegnungszonen)
 
 G_i=tp.create_poly_with_indices(intersection, path_strassenplan)
 print(type(G_i))
 G=gpd.GeoDataFrame(G_i)
-
 
 # create network from that bounding box
 #G1 = ox.graph_from_place('St. Gallen, Switzerland', simplify=False, network_type='drive_service')
