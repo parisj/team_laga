01708d46e21b2d3f8700d81f3dd96bd655249707
Alexander Bruun <alex@bruun.xyz>
2022-11-12 23:09:51 +0100

    Some changes

--- code/plot.py
+++ code/plot.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 from pyproj import Transformer
 
-plt.style.use('dark_background')
+# plt.style.use('dark_background')
 transformer = Transformer.from_crs('epsg:4326', 'epsg:3857')
 
-def plot_multipoly(multipoly):
+def plot_multipoly(multipoly, c='w'):
     fig, ax = plt.subplots()
     for poly in multipoly:
         tmp = poly.exterior.xy
         x, y = transformer.transform(tmp[1], tmp[0])
-        ax.plot(x, y, c='#008f11', lw=0.5)
+        ax.plot(x, y, c=c, lw=0.1)
     ax.axis('off')
     # ax.xaxis.set_visible(False)
     # ax.yaxis.set_visible(False)
@@ -23,6 +23,6 @@ if __name__ == "__main__":
     path_begegnungszonen = "data/begegnungszonen.csv"
     intersection = tp.import_intersection(path_strassenplan, path_data_30, path_begegnungszonen)
     multi_p = tp.create_multipolygon(path_strassenplan, area=False)
-    fig, ax = plot_multipoly(multi_p)
+    fig, ax = plot_multipoly(multi_p, '#008f11')
     fig.savefig("../plots/title_picture.pdf")
-    plt.show()
+    # plt.show()
