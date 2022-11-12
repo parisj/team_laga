4aa96183589980c51c2bc949392edb3c4966f879
Anton Paris <anton.j.paris@gmail.com>
2022-11-12 23:09:09 +0100

    working plot

--- code/visualize.py
+++ code/visualize.py
@@ -10,13 +10,20 @@ path_begegnungszonen = "code/data/begegnungszonen.csv"
 intersection = tp.import_intersection(path_strassenplan, path_begegnungszonen)
 
 G_i=tp.create_poly_with_indices(intersection, path_strassenplan)
-print(type(G_i))
-G=gpd.GeoDataFrame(G_i)
+#G_i = tp.create_multipolygon(path_begegnungszonen)
+#print(type(G_i))
+#G=gpd.GeoDataFrame(G_i)
 
 
-# create network from that bounding box
-#G1 = ox.graph_from_place('St. Gallen, Switzerland', simplify=False, network_type='drive_service')
 
-fig, ax = ox.graph_from_polygon(G_i)
+G = ox.graph_from_place('St. Gallen, Switzerland', simplify=True, network_type='drive_service')
+fig, ax = ox.plot_graph(G, node_size=0, show=False)
 
+breakpoint()
+for poly in G_i: 
+    #Scale and shif polygono points
+    #tmp = poly.exterior.xy
+    #x, y = transformer.transform(tmp[1], tmp[0])
+    x, y = poly.exterior.xy
+    ax.plot(x, y, c='#008f11')
 plt.show()
