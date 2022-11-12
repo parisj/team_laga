55a5e278ce6b2498ee1a937bd7e23d6c3e308885
Anton Paris <anton.j.paris@gmail.com>
2022-11-13 00:56:31 +0100

    Commenting

--- code/tools_osmnx.py
+++ code/tools_osmnx.py
@@ -5,6 +5,23 @@ from descartes.patch import PolygonPatch
 from shapely.geometry import Polygon, MultiPolygon
 
 def ax_patch(ax, polygon, fc, ec):
+    """
+   Takes polygon or Multipolygon and existing axis, plots 
+   polygons onto given axix
+   Polygon ploted with Face Color and Edge Color 
+    ----------
+     ax: Matplotlib axis
+     polygon: shapely.geometrie Polygon or Multipolygon
+     fc: Color Value
+     ec: Color Value
+    
+     file : path to csv file from OSM
+    
+    
+    Returns
+    -------
+    n no return 
+    """
     
     if polygon.geom_type == 'MultiPolygon':
         for poly in polygon:
@@ -16,7 +33,24 @@ def ax_patch(ax, polygon, fc, ec):
 
     return 0
 
+
 def strd_osmnx_plot(town,city, **kwargs):
+    """
+    import file path of streets based on indices
+    and create Multipoly    
+    Parameters
+    ----------
+     indices: indices of Geo Shapes
+
+    
+     file : path to csv file from OSM
+    
+    
+    Returns
+    -------
+    poly_indices: shapely.geometrix Multipolynom
+    """
+    
     
     G = ox.graph_from_place(town, city, simplify=True, network_type='drive_service')
     fig, ax = ox.plot_graph(G, node_size=0, show=False)
@@ -24,6 +58,20 @@ def strd_osmnx_plot(town,city, **kwargs):
     return fig, ax
 
 def point_osmnx_plot(coordinate, distance, **kwargs):
+    """
+    return fig, ax from a osmnx plot with at wanted coordinantes
+    with a distance, more options for 
+    ----------
+     indices: indices of Geo Shapes
+
+    
+     file : path to csv file from OSM
+    
+    
+    Returns
+    -------
+    poly_indices: shapely.geometrix Multipolynom
+    """
     
     G= ox.graph_from_point(coordinate, dist=distance, network_type='drive_service')
     fig, ax = ox.plot_graph(G, node_size=0, show=False)
