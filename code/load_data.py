7feea8d91153ab614e1776e478db19aa3007d464
Alexander Bruun <alex@bruun.xyz>
2022-11-12 12:40:59 +0100

    Some changes

--- code/load_data.py
+++ code/load_data.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 
 from shapely.geometry import Polygon
-from shapely.geometry import LineString
-from shapely.geometry.polygon import LinearRing
 
 from centerline.geometry import Centerline
 import geopandas as gpd
@@ -16,74 +14,55 @@ import warnings
 from shapely.errors import ShapelyDeprecationWarning
 warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
 
-# import osmnx
-# import osmnx as ox
-# import geopandas as gpd
-
-# Get place boundary related to the place name as a geodataframe
-# area = ox.graph_from_address("Hochwachtstrasse, St. Gallen, Switzerland")
-# ox.plot_graph(area, save=True, filepath="test.png", show=Falsew)
-
-# breakpoint()
-
-# from osmapi import OsmApi
-# MyApi = OsmApi()
+from pyproj import Proj, transform
+P3857 = Proj(init='epsg:3857')
+P4326 = Proj(init='epsg:4326')
 
 zonen_30 = pd.read_csv("data/tempo-30-zonen.csv", sep=";")
 begegnungszonen = pd.read_csv("data/begegnungszonen.csv", sep=";")
 gemeindestrassen = pd.read_csv("data/gemeindestrassenplan.csv", sep=";")
 
+# Average radius of earth [m] (±10km)
 r = 6371000
 
-# breakpoint()
-
-begegnungszonen_js = json.loads(gemeindestrassen['Geo Shape'][1])
-data = np.array(begegnungszonen_js['coordinates'][0])
-
-x = r * np.sin(np.radians(data[:, 0])) * np.cos(np.radians(data[:, 1]))
-y = r * np.sin(np.radians(data[:, 0])) * np.sin(np.radians(data[:, 1]))
-z = r * np.cos(np.radians(data[:, 0]))
-
-data = np.array([xyz for xyz in zip(x, y, z)])
-line = LinearRing(data)
-
-# attributes = {"id": 1, "name": "polygon", "valid": True}
-
-polygon = Polygon(data)
-centerline = Centerline(polygon)
-p = gpd.GeoDataFrame(centerline)
-p = p.set_geometry(0)
-
-print(p.distance(line).max())
-
 max_distance = []
-for i in range(0, gemeindestrassen.shape[0]):
+# for i in range(0, gemeindestrassen.shape[0]):
+for i in range(0, 1):
+    # Load data
     js = json.loads(gemeindestrassen['Geo Shape'][i])
-    data = np.array(js['coordinates'][0])
+    tmp = np.array(js['coordinates'][0])
 
-    x = r * np.sin(np.radians(data[:, 0])) * np.cos(np.radians(data[:, 1]))
-    y = r * np.sin(np.radians(data[:, 0])) * np.sin(np.radians(data[:, 1]))
-    z = r * np.cos(np.radians(data[:, 0]))
+    # Convert (lat,lon) to (x,y,z). (Formula could potentially be improved)
+    # x = r * np.sin(np.radians(tmp[:, 0])) * np.cos(np.radians(tmp[:, 1]))
+    # y = r * np.sin(np.radians(tmp[:, 0])) * np.sin(np.radians(tmp[:, 1]))
+    # z = r * np.cos(np.radians(tmp[:, 0]))
+    # data = np.array([xyz for xyz in zip(x, y, z)])
 
-    data = np.array([xyz for xyz in zip(x, y, z)])
-    line = LinearRing(data)
-
+    x, y = transform(P4326, P3857, tmp[:,0], tmp[:,1])
+    data = np.array([xyz for xyz in zip(x, y)])
+    
+    # Generic polygon shape of data (has interiors and exteriors)
     polygon = Polygon(data)
-    print(i, data.shape)
+
+    fig, ax = plt.subplots()
+    ax.plot(data[:, 0], data[:, 1], ls="-", marker="o")
+    fig.savefig("/tmp/test.png")
+
+    # print(gemeindestrassen["strassenna"].loc[i])
+    # print(gemeindestrassen["strassenkl"].loc[i])
+    # print(gemeindestrassen["strassennr"].loc[i])
+    
     try:
         centerline = Centerline(polygon)
         p = gpd.GeoDataFrame(centerline)
         p = p.set_geometry(0)
 
-        max_distance.append(p.distance(line).max())
+        max_distance.append(p.distance(Polygon.exterior).max())
+        print(max_distance[-1])
     except:
         print(i)
 
 breakpoint()
-
-# breakpoint()
-# breakpoint()
-# print(line.area)
 
 # # X, Y, Z = np.meshgrid(x, y, z)
 # # breakpoint()
@@ -98,5 +77,3 @@ breakpoint()
 # # surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
 # #                        linewidth=0, antialiased=False)
 # fig.savefig("/tmp/test.png")
-
-# breakpoint()
